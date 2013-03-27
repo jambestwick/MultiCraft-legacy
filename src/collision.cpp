minetest/minetest

Closed add object <-> object collision handling

src/collision.cpp
@@ -23,7 +23,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "nodedef.h"
 #include "gamedef.h"
 #include "log.h"
+#include "environment.h"
+#include "serverobject.h"
 #include <vector>
+#include <set>
 #include "util/timetaker.h"
 #include "main.h" // g_profiler
 #include "profiler.h"
@@ -186,11 +189,12 @@ bool wouldCollideWithCeiling(
 }
 
 
-collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
+collisionMoveResult collisionMoveSimple(Environment* env, IGameDef* gamedef,
 		f32 pos_max_d, const aabb3f &box_0,
 		f32 stepheight, f32 dtime,
 		v3f &pos_f, v3f &speed_f, v3f &accel_f)
 {
+	Map* map = &env->getMap();
 	//TimeTaker tt("collisionMoveSimple");
     ScopeProfiler sp(g_profiler, "collisionMoveSimple avg", SPT_AVG);
 
@@ -211,6 +215,7 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 	std::vector<aabb3f> cboxes;
 	std::vector<bool> is_unloaded;
 	std::vector<bool> is_step_up;
+	std::vector<bool> is_object;
 	std::vector<int> bouncy_values;
 	std::vector<v3s16> node_positions;
 	{
@@ -252,6 +257,7 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 				is_step_up.push_back(false);
 				bouncy_values.push_back(n_bouncy_value);
 				node_positions.push_back(p);
+				is_object.push_back(false);
 			}
 		}
 		catch(InvalidPositionException &e)
@@ -263,14 +269,72 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 			is_step_up.push_back(false);
 			bouncy_values.push_back(0);
 			node_positions.push_back(p);
+			is_object.push_back(false);
 		}
 	}
 	} // tt2
+
+	{
+		ScopeProfiler sp(g_profiler, "collisionMoveSimple objects avg", SPT_AVG);
+		//TimeTaker tt3("collisionMoveSimple collect object boxes");
+
+		/* add object boxes to cboxes */
+
+
+		core::list<ActiveObject*> objects;
+#ifndef SERVER
+		ClientEnvironment* c_env = dynamic_cast<ClientEnvironment*>(env);
+		if (c_env != 0)
+		{
+			f32 distance = speed_f.getLength();
+			core::array<DistanceSortedActiveObject> clientobjects;
+			c_env->getActiveObjects(pos_f,distance * 1.5,clientobjects);
+			for (int i=0; i < clientobjects.size(); i++)
+			{
+				objects.push_back((ActiveObject*)clientobjects[i].obj);
+			}
+		}
+		else
+#endif
+		{
+			ServerEnvironment* s_env = dynamic_cast<ServerEnvironment*>(env);
+			if (s_env != 0)
+			{
+				f32 distance = speed_f.getLength();
+				std::set<u16> s_objects = s_env->getObjectsInsideRadius(pos_f,distance * 1.5);
+				for (std::set<u16>::iterator iter = s_objects.begin(); iter != s_objects.end(); iter++)
+				{
+					ServerActiveObject* current = s_env->getActiveObject(*iter);
+					objects.push_back((ActiveObject*)current);
+				}
+			}
+		}
+
+		for (core::list<ActiveObject*>::Iterator iter = objects.begin();iter != objects.end(); iter++)
+		{
+			ActiveObject* object = *iter;
+
+			if (object != NULL)
+			{
+				aabb3f object_collisionbox;
+				if (object->getCollisionBox(&object_collisionbox))
+				{
+					cboxes.push_back(object_collisionbox);
+					is_unloaded.push_back(false);
+					is_step_up.push_back(false);
+					bouncy_values.push_back(0);
+					node_positions.push_back(v3s16(0,0,0));
+					is_object.push_back(true);
+				}
+			}
+		}
+	} //tt3
 
 	assert(cboxes.size() == is_unloaded.size());
 	assert(cboxes.size() == is_step_up.size());
 	assert(cboxes.size() == bouncy_values.size());
 	assert(cboxes.size() == node_positions.size());
+	assert(cboxes.size() == is_object.size());
 
 	/*
 		Collision detection
@@ -382,7 +446,11 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 				is_collision = false;
 
 			CollisionInfo info;
-			info.type = COLLISION_NODE;
+			if (is_object[nearest_boxindex]) {
+				info.type = COLLISION_OBJECT;
+			}
+			else
+				info.type = COLLISION_NODE;
 			info.node_p = node_positions[nearest_boxindex];
 			info.bouncy = bouncy;
 			info.old_speed = speed_f;

src/localplayer.cpp
@@ -25,6 +25,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "gamedef.h"
 #include "nodedef.h"
 #include "settings.h"
+#include "environment.h"
 #include "map.h"
 #include "util/numeric.h"
 
@@ -57,9 +58,10 @@ LocalPlayer::~LocalPlayer()
 {
 }
 
-void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
+void LocalPlayer::move(f32 dtime, ClientEnvironment* env, f32 pos_max_d,
 		core::list<CollisionInfo> *collision_info)
 {
+	Map *map = &env->getMap();
 	INodeDefManager *nodemgr = m_gamedef->ndef();
 
 	v3f position = getPosition();
@@ -97,13 +99,13 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		if(in_water)
 		{
 			v3s16 pp = floatToInt(position + v3f(0,BS*0.1,0), BS);
-			in_water = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
+			in_water = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
 		}
 		// If not in water, the threshold of going in is at lower y
 		else
 		{
 			v3s16 pp = floatToInt(position + v3f(0,BS*0.5,0), BS);
-			in_water = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
+			in_water = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
 		}
 	}
 	catch(InvalidPositionException &e)
@@ -116,7 +118,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	*/
 	try{
 		v3s16 pp = floatToInt(position + v3f(0,0,0), BS);
-		in_water_stable = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
+		in_water_stable = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
 	}
 	catch(InvalidPositionException &e)
 	{
@@ -130,8 +132,8 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	try {
 		v3s16 pp = floatToInt(position + v3f(0,0.5*BS,0), BS);
 		v3s16 pp2 = floatToInt(position + v3f(0,-0.2*BS,0), BS);
-		is_climbing = ((nodemgr->get(map.getNode(pp).getContent()).climbable ||
-		nodemgr->get(map.getNode(pp2).getContent()).climbable) && !free_move);
+		is_climbing = ((nodemgr->get(map->getNode(pp).getContent()).climbable ||
+		nodemgr->get(map->getNode(pp2).getContent()).climbable) && !free_move);
 	}
 	catch(InvalidPositionException &e)
 	{
@@ -195,7 +197,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 
 	v3f accel_f = v3f(0,0,0);
 
-	collisionMoveResult result = collisionMoveSimple(&map, m_gamedef,
+	collisionMoveResult result = collisionMoveSimple(env, m_gamedef,
 			pos_max_d, playerbox, player_stepheight, dtime,
 			position, m_speed, accel_f);
 
@@ -217,7 +219,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	*/
 	v3s16 current_node = floatToInt(position - v3f(0,BS/2,0), BS);
 	if(m_sneak_node_exists &&
-	   nodemgr->get(map.getNodeNoEx(m_old_node_below)).name == "air" &&
+	   nodemgr->get(map->getNodeNoEx(m_old_node_below)).name == "air" &&
 	   m_old_node_below_type != "air")
 	{
 		// Old node appears to have been removed; that is,
@@ -225,7 +227,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		m_need_to_get_new_sneak_node = false;
 		m_sneak_node_exists = false;
 	}
-	else if(nodemgr->get(map.getNodeNoEx(current_node)).name != "air")
+	else if(nodemgr->get(map->getNodeNoEx(current_node)).name != "air")
 	{
 		// We are on something, so make sure to recalculate the sneak
 		// node.
@@ -265,10 +267,10 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 
 			try{
 				// The node to be sneaked on has to be walkable
-				if(nodemgr->get(map.getNode(p)).walkable == false)
+				if(nodemgr->get(map->getNode(p)).walkable == false)
 					continue;
 				// And the node above it has to be nonwalkable
-				if(nodemgr->get(map.getNode(p+v3s16(0,1,0))).walkable == true)
+				if(nodemgr->get(map->getNode(p+v3s16(0,1,0))).walkable == true)
 					continue;
 			}
 			catch(InvalidPositionException &e)
@@ -329,7 +331,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	{
 		camera_barely_in_ceiling = false;
 		v3s16 camera_np = floatToInt(getEyePosition(), BS);
-		MapNode n = map.getNodeNoEx(camera_np);
+		MapNode n = map->getNodeNoEx(camera_np);
 		if(n.getContent() != CONTENT_IGNORE){
 			if(nodemgr->get(n).walkable && nodemgr->get(n).solidness == 2){
 				camera_barely_in_ceiling = true;
@@ -341,21 +343,21 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		Update the node last under the player
 	*/
 	m_old_node_below = floatToInt(position - v3f(0,BS/2,0), BS);
-	m_old_node_below_type = nodemgr->get(map.getNodeNoEx(m_old_node_below)).name;
+	m_old_node_below_type = nodemgr->get(map->getNodeNoEx(m_old_node_below)).name;
 	
 	/*
 		Check properties of the node on which the player is standing
 	*/
-	const ContentFeatures &f = nodemgr->get(map.getNodeNoEx(getStandingNodePos()));
+	const ContentFeatures &f = nodemgr->get(map->getNodeNoEx(getStandingNodePos()));
 	// Determine if jumping is possible
 	m_can_jump = touching_ground;
 	if(itemgroup_get(f.groups, "disable_jump"))
 		m_can_jump = false;
 }
 
-void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d)
+void LocalPlayer::move(f32 dtime, ClientEnvironment* env, f32 pos_max_d)
 {
-	move(dtime, map, pos_max_d, NULL);
+	move(dtime, env, pos_max_d, NULL);
 }
 
 void LocalPlayer::applyControl(float dtime)

jambestwick/MultiCraft-legacy

src/collision.cpp
@@ -23,7 +23,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "nodedef.h"
 #include "gamedef.h"
 #include "log.h"
+#include "environment.h"
+#include "serverobject.h"
 #include <vector>
+#include <set>
 #include "util/timetaker.h"
 #include "main.h" // g_profiler
 #include "profiler.h"
@@ -186,11 +189,12 @@ bool wouldCollideWithCeiling(
 }
 
 
-collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
+collisionMoveResult collisionMoveSimple(Environment *env, IGameDef *gamedef,
 		f32 pos_max_d, const aabb3f &box_0,
 		f32 stepheight, f32 dtime,
 		v3f &pos_f, v3f &speed_f, v3f &accel_f)
 {
+	Map *map = &env->getMap();
 	//TimeTaker tt("collisionMoveSimple");
     ScopeProfiler sp(g_profiler, "collisionMoveSimple avg", SPT_AVG);
 
@@ -215,6 +219,7 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 	std::vector<aabb3f> cboxes;
 	std::vector<bool> is_unloaded;
 	std::vector<bool> is_step_up;
+	std::vector<bool> is_object;
 	std::vector<int> bouncy_values;
 	std::vector<v3s16> node_positions;
 	{
@@ -256,6 +261,7 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 				is_step_up.push_back(false);
 				bouncy_values.push_back(n_bouncy_value);
 				node_positions.push_back(p);
+				is_object.push_back(false);
 			}
 		}
 		catch(InvalidPositionException &e)
@@ -267,14 +273,72 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 			is_step_up.push_back(false);
 			bouncy_values.push_back(0);
 			node_positions.push_back(p);
+			is_object.push_back(false);
 		}
 	}
 	} // tt2
+
+	{
+		ScopeProfiler sp(g_profiler, "collisionMoveSimple objects avg", SPT_AVG);
+		//TimeTaker tt3("collisionMoveSimple collect object boxes");
+
+		/* add object boxes to cboxes */
+
+
+		std::list<ActiveObject*> objects;
+#ifndef SERVER
+		ClientEnvironment *c_env = dynamic_cast<ClientEnvironment*>(env);
+		if (c_env != 0)
+		{
+			f32 distance = speed_f.getLength();
+			std::vector<DistanceSortedActiveObject> clientobjects;
+			c_env->getActiveObjects(pos_f,distance * 1.5,clientobjects);
+			for (int i=0; i < clientobjects.size(); i++)
+			{
+				objects.push_back((ActiveObject*)clientobjects[i].obj);
+			}
+		}
+		else
+#endif
+		{
+			ServerEnvironment *s_env = dynamic_cast<ServerEnvironment*>(env);
+			if (s_env != 0)
+			{
+				f32 distance = speed_f.getLength();
+				std::set<u16> s_objects = s_env->getObjectsInsideRadius(pos_f,distance * 1.5);
+				for (std::set<u16>::iterator iter = s_objects.begin(); iter != s_objects.end(); iter++)
+				{
+					ServerActiveObject *current = s_env->getActiveObject(*iter);
+					objects.push_back((ActiveObject*)current);
+				}
+			}
+		}
+
+		for (std::list<ActiveObject*>::const_iterator iter = objects.begin();iter != objects.end(); ++iter)
+		{
+			ActiveObject *object = *iter;
+
+			if (object != NULL)
+			{
+				aabb3f object_collisionbox;
+				if (object->getCollisionBox(&object_collisionbox))
+				{
+					cboxes.push_back(object_collisionbox);
+					is_unloaded.push_back(false);
+					is_step_up.push_back(false);
+					bouncy_values.push_back(0);
+					node_positions.push_back(v3s16(0,0,0));
+					is_object.push_back(true);
+				}
+			}
+		}
+	} //tt3
 
 	assert(cboxes.size() == is_unloaded.size());
 	assert(cboxes.size() == is_step_up.size());
 	assert(cboxes.size() == bouncy_values.size());
 	assert(cboxes.size() == node_positions.size());
+	assert(cboxes.size() == is_object.size());
 
 	/*
 		Collision detection
@@ -386,7 +450,11 @@ collisionMoveResult collisionMoveSimple(Map *map, IGameDef *gamedef,
 				is_collision = false;
 
 			CollisionInfo info;
-			info.type = COLLISION_NODE;
+			if (is_object[nearest_boxindex]) {
+				info.type = COLLISION_OBJECT;
+			}
+			else
+				info.type = COLLISION_NODE;
 			info.node_p = node_positions[nearest_boxindex];
 			info.bouncy = bouncy;
 			info.old_speed = speed_f;

src/localplayer.cpp
@@ -25,6 +25,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "gamedef.h"
 #include "nodedef.h"
 #include "settings.h"
+#include "environment.h"
 #include "map.h"
 #include "util/numeric.h"
 
@@ -57,9 +58,10 @@ LocalPlayer::~LocalPlayer()
 {
 }
 
-void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
+void LocalPlayer::move(f32 dtime, ClientEnvironment *env, f32 pos_max_d,
 		std::list<CollisionInfo> *collision_info)
 {
+	Map *map = &env->getMap();
 	INodeDefManager *nodemgr = m_gamedef->ndef();
 
 	v3f position = getPosition();
@@ -97,15 +99,15 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		if(in_liquid)
 		{
 			v3s16 pp = floatToInt(position + v3f(0,BS*0.1,0), BS);
-			in_liquid = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
-			liquid_viscosity = nodemgr->get(map.getNode(pp).getContent()).liquid_viscosity;
+			in_liquid = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
+			liquid_viscosity = nodemgr->get(map->getNode(pp).getContent()).liquid_viscosity;
 		}
 		// If not in liquid, the threshold of going in is at lower y
 		else
 		{
 			v3s16 pp = floatToInt(position + v3f(0,BS*0.5,0), BS);
-			in_liquid = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
-			liquid_viscosity = nodemgr->get(map.getNode(pp).getContent()).liquid_viscosity;
+			in_liquid = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
+			liquid_viscosity = nodemgr->get(map->getNode(pp).getContent()).liquid_viscosity;
 		}
 	}
 	catch(InvalidPositionException &e)
@@ -118,7 +120,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	*/
 	try{
 		v3s16 pp = floatToInt(position + v3f(0,0,0), BS);
-		in_liquid_stable = nodemgr->get(map.getNode(pp).getContent()).isLiquid();
+		in_liquid_stable = nodemgr->get(map->getNode(pp).getContent()).isLiquid();
 	}
 	catch(InvalidPositionException &e)
 	{
@@ -132,8 +134,8 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	try {
 		v3s16 pp = floatToInt(position + v3f(0,0.5*BS,0), BS);
 		v3s16 pp2 = floatToInt(position + v3f(0,-0.2*BS,0), BS);
-		is_climbing = ((nodemgr->get(map.getNode(pp).getContent()).climbable ||
-		nodemgr->get(map.getNode(pp2).getContent()).climbable) && !free_move);
+		is_climbing = ((nodemgr->get(map->getNode(pp).getContent()).climbable ||
+		nodemgr->get(map->getNode(pp2).getContent()).climbable) && !free_move);
 	}
 	catch(InvalidPositionException &e)
 	{
@@ -197,7 +199,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 
 	v3f accel_f = v3f(0,0,0);
 
-	collisionMoveResult result = collisionMoveSimple(&map, m_gamedef,
+	collisionMoveResult result = collisionMoveSimple(env, m_gamedef,
 			pos_max_d, playerbox, player_stepheight, dtime,
 			position, m_speed, accel_f);
 
@@ -219,7 +221,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	*/
 	v3s16 current_node = floatToInt(position - v3f(0,BS/2,0), BS);
 	if(m_sneak_node_exists &&
-	   nodemgr->get(map.getNodeNoEx(m_old_node_below)).name == "air" &&
+	   nodemgr->get(map->getNodeNoEx(m_old_node_below)).name == "air" &&
 	   m_old_node_below_type != "air")
 	{
 		// Old node appears to have been removed; that is,
@@ -227,7 +229,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		m_need_to_get_new_sneak_node = false;
 		m_sneak_node_exists = false;
 	}
-	else if(nodemgr->get(map.getNodeNoEx(current_node)).name != "air")
+	else if(nodemgr->get(map->getNodeNoEx(current_node)).name != "air")
 	{
 		// We are on something, so make sure to recalculate the sneak
 		// node.
@@ -267,10 +269,10 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 
 			try{
 				// The node to be sneaked on has to be walkable
-				if(nodemgr->get(map.getNode(p)).walkable == false)
+				if(nodemgr->get(map->getNode(p)).walkable == false)
 					continue;
 				// And the node above it has to be nonwalkable
-				if(nodemgr->get(map.getNode(p+v3s16(0,1,0))).walkable == true)
+				if(nodemgr->get(map->getNode(p+v3s16(0,1,0))).walkable == true)
 					continue;
 			}
 			catch(InvalidPositionException &e)
@@ -331,7 +333,7 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 	{
 		camera_barely_in_ceiling = false;
 		v3s16 camera_np = floatToInt(getEyePosition(), BS);
-		MapNode n = map.getNodeNoEx(camera_np);
+		MapNode n = map->getNodeNoEx(camera_np);
 		if(n.getContent() != CONTENT_IGNORE){
 			if(nodemgr->get(n).walkable && nodemgr->get(n).solidness == 2){
 				camera_barely_in_ceiling = true;
@@ -343,21 +345,21 @@ void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d,
 		Update the node last under the player
 	*/
 	m_old_node_below = floatToInt(position - v3f(0,BS/2,0), BS);
-	m_old_node_below_type = nodemgr->get(map.getNodeNoEx(m_old_node_below)).name;
+	m_old_node_below_type = nodemgr->get(map->getNodeNoEx(m_old_node_below)).name;
 	
 	/*
 		Check properties of the node on which the player is standing
 	*/
-	const ContentFeatures &f = nodemgr->get(map.getNodeNoEx(getStandingNodePos()));
+	const ContentFeatures &f = nodemgr->get(map->getNodeNoEx(getStandingNodePos()));
 	// Determine if jumping is possible
 	m_can_jump = touching_ground && !in_liquid;
 	if(itemgroup_get(f.groups, "disable_jump"))
 		m_can_jump = false;
 }
 
-void LocalPlayer::move(f32 dtime, Map &map, f32 pos_max_d)
+void LocalPlayer::move(f32 dtime, ClientEnvironment *env, f32 pos_max_d)
 {
-	move(dtime, map, pos_max_d, NULL);
+	move(dtime, env, pos_max_d, NULL);
 }
 
 void LocalPlayer::applyControl(float dtime)