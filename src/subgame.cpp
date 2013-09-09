minebest/minetest

Add dummy and LevelDB database backends

src/subgame.cpp
@@ -219,7 +219,7 @@ bool initializeWorld(const std::string &path, const std::string &gameid)
 		infostream<<"Creating world.mt ("<<worldmt_path<<")"<<std::endl;
 		fs::CreateAllDirs(path);
 		std::ofstream of(worldmt_path.c_str(), std::ios::binary);
-		of<<"gameid = "<<gameid<<"\n";
+		of<<"gameid = "<<gameid<<"\nbackend = sqlite3";
 	}
 	return true;
 }

jambestwick/MultiCraft-legacy

src/subgame.cpp
@@ -242,7 +242,7 @@ bool initializeWorld(const std::string &path, const std::string &gameid)
 		infostream<<"Creating world.mt ("<<worldmt_path<<")"<<std::endl;
 		fs::CreateAllDirs(path);
 		std::ostringstream ss(std::ios_base::binary);
-		ss<<"gameid = "<<gameid<<"\n";
+		ss<<"gameid = "<<gameid<<"\nbackend = sqlite3";
 		fs::safeWriteToFile(worldmt_path, ss.str());
 	}
 	return true;