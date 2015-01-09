minetest/minetest

Fix memory leaks due to messed up memory handling for particles as well as their spawners

src/client.h
@@ -454,6 +454,7 @@ public:
 	virtual u16 allocateUnknownNodeId(const std::string &name);
 	virtual ISoundManager* getSoundManager();
 	virtual MtEventManager* getEventManager();
+	virtual ParticleManager* getParticleManager();
 	virtual bool checkLocalPrivilege(const std::string &priv)
 	{ return checkPrivilege(priv); }
 	virtual scene::IAnimatedMesh* getMesh(const std::string &filename);
@@ -497,8 +498,10 @@ private:
 	ISoundManager *m_sound;
 	MtEventManager *m_event;
 
+
 	MeshUpdateThread m_mesh_update_thread;
 	ClientEnvironment m_env;
+	ParticleManager m_particlemanager;
 	con::Connection m_con;
 	IrrlichtDevice *m_device;
 	// Server serialization version

src/particles.h
@@ -28,6 +28,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "localplayer.h"
 #include "environment.h"
 
+struct ClientEvent;
+class ParticleManager;
+
 class Particle : public scene::ISceneNode
 {
 	public:
@@ -35,7 +38,7 @@ class Particle : public scene::ISceneNode
 		IGameDef* gamedef,
 		scene::ISceneManager* mgr,
 		LocalPlayer *player,
-		ClientEnvironment &env,
+		ClientEnvironment *env,
 		v3f pos,
 		v3f velocity,
 		v3f acceleration,
@@ -114,16 +117,18 @@ class ParticleSpawner
 		bool collisiondetection,
 		bool vertical,
 		video::ITexture *texture,
-		u32 id);
+		u32 id,
+		ParticleManager* p_manager);
 
 	~ParticleSpawner();
 
-	void step(float dtime, ClientEnvironment &env);
+	void step(float dtime, ClientEnvironment *env);
 
 	bool get_expired ()
 	{ return (m_amount <= 0) && m_spawntime != 0; }
 
 	private:
+	ParticleManager* m_particlemanager;
 	float m_time;
 	IGameDef *m_gamedef;
 	scene::ISceneManager *m_smgr;
@@ -144,24 +149,49 @@ class ParticleSpawner
 	std::vector<float> m_spawntimes;
 	bool m_collisiondetection;
 	bool m_vertical;
+
 };
 
-void allparticles_step (float dtime);
-void allparticlespawners_step (float dtime, ClientEnvironment &env);
+/**
+ * Class doing particle as well as their spawners handling
+ */
+class ParticleManager
+{
+friend class ParticleSpawner;
+public:
+	ParticleManager(ClientEnvironment* env);
+	~ParticleManager();
 
-void delete_particlespawner (u32 id);
-void clear_particles ();
+	void step (float dtime);
 
-void addDiggingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void handleParticleEvent(ClientEvent *event,IGameDef *gamedef,
+			scene::ISceneManager* smgr, LocalPlayer *player);
 
-void addPunchingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void addDiggingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
 
-void addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void addPunchingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
+
+	void addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
+
+protected:
+	void addParticle(Particle* toadd);
+
+private:
+
+	void stepParticles (float dtime);
+	void stepSpawners (float dtime);
+
+	void clearAll ();
+
+	std::vector<Particle*> m_particles;
+	std::map<u32, ParticleSpawner*> m_particlespawners;
+
+	ClientEnvironment* m_env;
+	JMutex m_particlelistlock;
+	JMutex m_spawnerlistlock;
+};
 
 #endif

jambestwick/MultiCraft-legacy

src/client.h
@@ -454,6 +454,7 @@ public:
 	virtual u16 allocateUnknownNodeId(const std::string &name);
 	virtual ISoundManager* getSoundManager();
 	virtual MtEventManager* getEventManager();
+	virtual ParticleManager* getParticleManager();
 	virtual bool checkLocalPrivilege(const std::string &priv)
 	{ return checkPrivilege(priv); }
 	virtual scene::IAnimatedMesh* getMesh(const std::string &filename);
@@ -497,8 +498,10 @@ private:
 	ISoundManager *m_sound;
 	MtEventManager *m_event;
 
+
 	MeshUpdateThread m_mesh_update_thread;
 	ClientEnvironment m_env;
+	ParticleManager m_particle_manager;
 	con::Connection m_con;
 	IrrlichtDevice *m_device;
 	// Server serialization version

src/particles.h
@@ -28,6 +28,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "localplayer.h"
 #include "environment.h"
 
+struct ClientEvent;
+class ParticleManager;
+
 class Particle : public scene::ISceneNode
 {
 	public:
@@ -35,7 +38,7 @@ class Particle : public scene::ISceneNode
 		IGameDef* gamedef,
 		scene::ISceneManager* mgr,
 		LocalPlayer *player,
-		ClientEnvironment &env,
+		ClientEnvironment *env,
 		v3f pos,
 		v3f velocity,
 		v3f acceleration,
@@ -114,16 +117,18 @@ class ParticleSpawner
 		bool collisiondetection,
 		bool vertical,
 		video::ITexture *texture,
-		u32 id);
+		u32 id,
+		ParticleManager* p_manager);
 
 	~ParticleSpawner();
 
-	void step(float dtime, ClientEnvironment &env);
+	void step(float dtime, ClientEnvironment *env);
 
 	bool get_expired ()
 	{ return (m_amount <= 0) && m_spawntime != 0; }
 
 	private:
+	ParticleManager* m_particlemanager;
 	float m_time;
 	IGameDef *m_gamedef;
 	scene::ISceneManager *m_smgr;
@@ -144,24 +149,49 @@ class ParticleSpawner
 	std::vector<float> m_spawntimes;
 	bool m_collisiondetection;
 	bool m_vertical;
+
 };
 
-void allparticles_step (float dtime);
-void allparticlespawners_step (float dtime, ClientEnvironment &env);
+/**
+ * Class doing particle as well as their spawners handling
+ */
+class ParticleManager
+{
+friend class ParticleSpawner;
+public:
+	ParticleManager(ClientEnvironment* env);
+	~ParticleManager();
 
-void delete_particlespawner (u32 id);
-void clear_particles ();
+	void step (float dtime);
 
-void addDiggingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void handleParticleEvent(ClientEvent *event,IGameDef *gamedef,
+			scene::ISceneManager* smgr, LocalPlayer *player);
 
-void addPunchingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void addDiggingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
 
-void addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
-	LocalPlayer *player, ClientEnvironment &env, v3s16 pos,
-	const TileSpec tiles[]);
+	void addPunchingParticles(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
+
+	void addNodeParticle(IGameDef* gamedef, scene::ISceneManager* smgr,
+		LocalPlayer *player, v3s16 pos, const TileSpec tiles[]);
+
+protected:
+	void addParticle(Particle* toadd);
+
+private:
+
+	void stepParticles (float dtime);
+	void stepSpawners (float dtime);
+
+	void clearAll ();
+
+	std::vector<Particle*> m_particles;
+	std::map<u32, ParticleSpawner*> m_particle_spawners;
+
+	ClientEnvironment* m_env;
+	JMutex m_particle_list_lock;
+	JMutex m_spawner_list_lock;
+};
 
 #endif