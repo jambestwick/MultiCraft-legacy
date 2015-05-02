68d4c67f08b03a3e264e84a278552a88a010e4da
OttoLidenbrock <unknown>
2015-05-03 00:35:22 +1000

    Change sdcard project dir from /minetest to /multicraft

--- src/config.h
+++ src/config.h
@@ -13,8 +13,8 @@
 #if defined USE_CMAKE_CONFIG_H
 	#include "cmake_config.h"
 #elif defined (__ANDROID__) || defined (ANDROID)
-	#define PROJECT_NAME "minetest"
-	#define PROJECT_NAME_C "Minetest"
+	#define PROJECT_NAME "multicraft"
+	#define PROJECT_NAME_C "MultiCraft"
 	#define STATIC_SHAREDIR ""
 	#include "android_version.h"
 	#ifdef NDEBUG
