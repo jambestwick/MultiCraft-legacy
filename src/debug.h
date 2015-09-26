minetest/minetest

commit ced6d20295a8263757d57c02a07ffcb66688a163

For usages of assert() that are meant to persist in Release builds (when NDEBUG is defined), replace those usages with persistent alternatives

src/debug.h
@@ -22,6 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include <iostream>
 #include <exception>
+#include <assert.h>
 #include "gettime.h"
 
 #if (defined(WIN32) || defined(_WIN32_WCE))
@@ -72,28 +73,38 @@ extern std::ostream dstream;
 extern std::ostream dstream_no_stderr;
 extern Nullstream dummyout;
 
+
+/* Abort program execution immediately
+ */
+__NORETURN extern void fatal_error_fn(
+		const char *msg, const char *file,
+		unsigned int line, const char *function);
+
+#define FATAL_ERROR(msg) \
+	fatal_error_fn((msg), __FILE__, __LINE__, __FUNCTION_NAME)
+
+#define FATAL_ERROR_IF(expr, msg) \
+	((expr) \
+	? fatal_error_fn((msg), __FILE__, __LINE__, __FUNCTION_NAME) \
+	: (void)(0))
+
 /*
-	Include assert.h and immediately undef assert so that it can't override
-	our assert later on. leveldb/slice.h is a notable offender.
+	sanity_check()
+	Equivalent to assert() but persists in Release builds (i.e. when NDEBUG is
+	defined)
 */
 
-#include <assert.h>
-#undef assert
-
-/*
-	Assert
-*/
-
-__NORETURN extern void assert_fail(
+__NORETURN extern void sanity_check_fn(
 		const char *assertion, const char *file,
 		unsigned int line, const char *function);
 
-#define ASSERT(expr)\
-	((expr)\
-	? (void)(0)\
-	: assert_fail(#expr, __FILE__, __LINE__, __FUNCTION_NAME))
+#define SANITY_CHECK(expr) \
+	((expr) \
+	? (void)(0) \
+	: sanity_check_fn(#expr, __FILE__, __LINE__, __FUNCTION_NAME))
 
-#define assert(expr) ASSERT(expr)
+#define sanity_check(expr) SANITY_CHECK(expr)
+
 
 void debug_set_exception_handler();
 

jambestwick/MultiCraft-legacy

commit 8cdb95a157b40d043caca4ec5cd26c6fefedae28

Abort at uncatched exceptions
Change a remaining assert(0) call to FATAL_ERROR(msg).
There was a regression since commit

ced6d20295a8263757d57c02a07ffcb66688a163 "For usages of assert() that are meant to persist in Release builds (when NDEBUG is defined), replace those usages with persistent alternatives"

where when an "uncatched" exception is thrown inside a "side thread",
the program doesn't abort anymore.

This led to the problem @netinetwalker experienced where the emergethread
got an unhandled exception for loading a mapblock while redis was loading,
(see #3196) and then jmped outside its loop to work down its queue.
This resulted in the server not doing any emerges anymore.

src/debug.h
@@ -42,7 +42,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #endif
 
 // Whether to catch all std::exceptions.
-// Assert will be called on such an event.
+// When "catching", the program will abort with an error message.
 // In debug mode, leave these for the debugger and don't catch them.
 #ifdef NDEBUG
 	#define CATCH_UNHANDLED_EXCEPTIONS 1
@@ -145,11 +145,11 @@ private:
 
 #if CATCH_UNHANDLED_EXCEPTIONS == 1
 	#define BEGIN_DEBUG_EXCEPTION_HANDLER try {
-	#define END_DEBUG_EXCEPTION_HANDLER(logstream) \
-		} catch (std::exception &e) {                               \
-			logstream << "ERROR: An unhandled exception occurred: " \
-				<< e.what() << std::endl;                           \
-			assert(0);                                              \
+	#define END_DEBUG_EXCEPTION_HANDLER(logstream)           \
+		} catch (std::exception &e) {                        \
+			logstream << "An unhandled exception occurred: " \
+				<< e.what() << std::endl;                    \
+			FATAL_ERROR(e.what());                           \
 		}
 #else
 	// Dummy ones