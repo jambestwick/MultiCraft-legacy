1aa03f98758d5c9f6096c661f3a1ff6f77aafaa2
Maksim Gamarnik <MoNTE48@mail.ua>
2016-02-17 07:13:12 +0200

    Android: SPEEEEED!

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -1,8 +1,3 @@
-# NDK_TOOLCHAIN_VERSION := clang3.3
-
 APP_PLATFORM := android-16
 APP_MODULES := multicraft
 APP_STL := gnustl_static
-
-APP_CPPFLAGS += -fexceptions
-APP_GNUSTL_FORCE_CPP_FEATURES := rtti
