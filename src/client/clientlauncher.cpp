minetest/minetest

commit 3b50b2766aeb09c9fc0ad0ea07426bb2187df3d7

Optional reconnect functionality
Enable the server to request the client to reconnect.

This can be done with the now extended minetest.request_shutdown([reason], [reconnect]) setting.

src/client/clientlauncher.cpp
@@ -168,8 +168,9 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 	ChatBackend chat_backend;
 
 	// If an error occurs, this is set to something by menu().
-	// It is then displayed before	the menu shows on the next call to menu()
+	// It is then displayed before the menu shows on the next call to menu()
 	std::string error_message;
+	bool reconnect_requested = false;
 
 	bool first_loop = true;
 
@@ -197,7 +198,8 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 			*/
 			guiroot = guienv->addStaticText(L"", core::rect<s32>(0, 0, 10000, 10000));
 
-			bool game_has_run = launch_game(error_message, game_params, cmd_args);
+			bool game_has_run = launch_game(error_message, reconnect_requested,
+				game_params, cmd_args);
 
 			// If skip_main_menu, we only want to startup once
 			if (skip_main_menu && !first_loop)
@@ -233,6 +235,7 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 			receiver->m_touchscreengui = new TouchScreenGUI(device, receiver);
 			g_touchscreengui = receiver->m_touchscreengui;
 #endif
+
 			the_game(
 				kill,
 				random_input,
@@ -245,6 +248,7 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 				current_port,
 				error_message,
 				chat_backend,
+				&reconnect_requested,
 				gamespec,
 				simple_singleplayer_mode
 			);
@@ -325,14 +329,16 @@ bool ClientLauncher::init_engine(int log_level)
 }
 
 bool ClientLauncher::launch_game(std::string &error_message,
-		GameParams &game_params, const Settings &cmd_args)
+		bool reconnect_requested, GameParams &game_params,
+		const Settings &cmd_args)
 {
 	// Initialize menu data
 	MainMenuData menudata;
-	menudata.address      = address;
-	menudata.name         = playername;
-	menudata.port         = itos(game_params.socket_port);
-	menudata.errormessage = error_message;
+	menudata.address                         = address;
+	menudata.name                            = playername;
+	menudata.port                            = itos(game_params.socket_port);
+	menudata.script_data.errormessage        = error_message;
+	menudata.script_data.reconnect_requested = reconnect_requested;
 
 	error_message.clear();
 
@@ -379,11 +385,11 @@ bool ClientLauncher::launch_game(std::string &error_message,
 		}
 	}
 
-	if (!menudata.errormessage.empty()) {
+	if (!menudata.script_data.errormessage.empty()) {
 		/* The calling function will pass this back into this function upon the
 		 * next iteration (if any) causing it to be displayed by the GUI
 		 */
-		error_message = menudata.errormessage;
+		error_message = menudata.script_data.errormessage;
 		return false;
 	}
 

builtin/fstk/ui.lua
@@ -54,29 +54,42 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+local function wordwrap_quickhack(str)
+	local res = ""
+	local ar = str:split("\n")
+	for i = 1, #ar do
+		local text = ar[i]
+		-- Hack to add word wrapping.
+		-- TODO: Add engine support for wrapping in formspecs
+		while #text > 80 do
+			if res ~= "" then
+				res = res .. ","
+			end
+			res = res .. core.formspec_escape(string.sub(text, 1, 79))
+			text = string.sub(text, 80, #text)
+		end
+		if res ~= "" then
+			res = res .. ","
+		end
+		res = res .. core.formspec_escape(text)
+	end
+	return res
+end
+
 --------------------------------------------------------------------------------
 function ui.update()
 	local formspec = ""
 
 	-- handle errors
-	if gamedata ~= nil and gamedata.errormessage ~= nil then
-		local ar = gamedata.errormessage:split("\n")
-		for i = 1, #ar do
-			local text = ar[i]
-			-- Hack to add word wrapping.
-			-- TODO: Add engine support for wrapping in formspecs
-			while #text > 80 do
-				if formspec ~= "" then
-					formspec = formspec .. ","
-				end
-				formspec = formspec .. core.formspec_escape(string.sub(text, 1, 79))
-				text = string.sub(text, 80, #text)
-			end
-			if formspec ~= "" then
-				formspec = formspec .. ","
-			end
-			formspec = formspec .. core.formspec_escape(text)
-		end
+	if gamedata ~= nil and gamedata.reconnect_requested then
+		formspec = wordwrap_quickhack(gamedata.errormessage or "")
+		formspec = "size[12,5]" ..
+				"label[0.5,0;" .. fgettext("The server has requested a reconnect:") ..
+				"]textlist[0.2,0.8;11.5,3.5;;" .. formspec ..
+				"]button[6,4.6;3,0.5;btn_reconnect_no;" .. fgettext("Main menu") .. "]" ..
+				"button[3,4.6;3,0.5;btn_reconnect_yes;" .. fgettext("Reconnect") .. "]"
+	elseif gamedata ~= nil and gamedata.errormessage ~= nil then
+		formspec = wordwrap_quickhack(gamedata.errormessage)
 		local error_title
 		if string.find(gamedata.errormessage, "ModError") then
 			error_title = fgettext("An error occured in a Lua script, such as a mod:")
@@ -128,13 +141,6 @@ end
 
 --------------------------------------------------------------------------------
 function ui.handle_buttons(fields)
-
-	if fields["btn_error_confirm"] then
-		gamedata.errormessage = nil
-		update_menu()
-		return
-	end
-
 	for key,value in pairs(ui.childlist) do
 
 		local retval = value:handle_buttons(fields)
@@ -168,8 +174,15 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 core.button_handler = function(fields)
-	if fields["btn_error_confirm"] then
+	if fields["btn_reconnect_yes"] then
+		gamedata.reconnect_requested = false
 		gamedata.errormessage = nil
+		gamedata.do_reconnect = true
+		core.start()
+		return
+	elseif fields["btn_reconnect_no"] or fields["btn_error_confirm"] then
+		gamedata.errormessage = nil
+		gamedata.reconnect_requested = false
 		ui.update()
 		return
 	end

minetest.conf.example
@@ -393,6 +393,9 @@
 #    A message to be displayed to all clients when the server shuts down
 #kick_msg_crash = This server has experienced an internal error. You will now be disconnected.
 #    A message to be displayed to all clients when the server crashes
+#ask_reconnect_on_crash = false
+#    Whether to ask clients to reconnect after a (lua) crash.
+#    Set this to true if your server is set up to restart automatically.
 
 #    Mod profiler
 #mod_profiling = false

src/client.cpp
@@ -244,6 +244,7 @@ Client::Client(
 	m_chosen_auth_mech(AUTH_MECHANISM_NONE),
 	m_auth_data(NULL),
 	m_access_denied(false),
+	m_access_denied_reconnect(false),
 	m_itemdef_received(false),
 	m_nodedef_received(false),
 	m_media_downloader(new ClientMediaDownloader()),

src/client.h
@@ -489,6 +489,8 @@ public:
 	bool accessDenied()
 	{ return m_access_denied; }
 
+	bool reconnectRequested() { return m_access_denied_reconnect; }
+
 	std::string accessDeniedReason()
 	{ return m_access_denied_reason; }
 
@@ -636,6 +638,7 @@ private:
 
 
 	bool m_access_denied;
+	bool m_access_denied_reconnect;
 	std::string m_access_denied_reason;
 	std::queue<ClientEvent> m_client_event_queue;
 	bool m_itemdef_received;

src/client/clientlauncher.h
@@ -92,8 +92,8 @@ protected:
 	void init_args(GameParams &game_params, const Settings &cmd_args);
 	bool init_engine(int log_level);
 
-	bool launch_game(std::string &error_message, GameParams &game_params,
-			const Settings &cmd_args);
+	bool launch_game(std::string &error_message, bool reconnect_requested,
+		GameParams &game_params, const Settings &cmd_args);
 
 	void main_menu(MainMenuData *menudata);
 	bool create_engine_device(int log_level);

src/defaultsettings.cpp
@@ -254,6 +254,7 @@ void set_default_settings(Settings *settings)
 
 	settings->setDefault("kick_msg_shutdown", "Server shutting down.");
 	settings->setDefault("kick_msg_crash", "This server has experienced an internal error. You will now be disconnected.");
+	settings->setDefault("ask_reconnect_on_crash", "false");
 
 	settings->setDefault("profiler_print_interval", "0");
 	settings->setDefault("enable_mapgen_debug_info", "false");

src/environment.cpp
@@ -426,13 +426,15 @@ bool ServerEnvironment::line_of_sight(v3f pos1, v3f pos2, float stepsize, v3s16 
 	return true;
 }
 
-void ServerEnvironment::kickAllPlayers(const std::string &reason)
+void ServerEnvironment::kickAllPlayers(AccessDeniedCode reason,
+		const std::string &str_reason, bool reconnect)
 {
-	std::wstring wreason = utf8_to_wide(reason);
 	for (std::vector<Player*>::iterator it = m_players.begin();
 			it != m_players.end();
 			++it) {
-		((Server*)m_gamedef)->DenyAccess_Legacy((*it)->peer_id, wreason);
+		((Server*)m_gamedef)->DenyAccessVerCompliant((*it)->peer_id,
+			(*it)->protocol_version, (AccessDeniedCode)reason,
+			str_reason, reconnect);
 	}
 }
 

jambestwick/MultiCraft-legacy

commit 0545d1445473375774b4ecf861c9322b7dc80783

Fix two reconnect bugs
Fix two bugs related to the reconnect feature
introduced by commit

3b50b2766aeb09c9fc0ad0ea07426bb2187df3d7 "Optional reconnect functionality"

1. Set the password to the stored one

Before, we have done the reconnect attempt with a
cleared password, so using the feature would only
work if you had an empty password.
Thanks to @orwell96 for reporting the bug.

2. Reset the reconnect_requested flag after its use

the_game only writes to the reconect_requested flag
if it sets it to true. It never sets it to false.
If the flag is not reset after its use, all "reset"s
to the main menu will look like the server had
requested a reconnect.

src/client/clientlauncher.cpp
@@ -201,6 +201,9 @@ bool ClientLauncher::run(GameParams &game_params, const Settings &cmd_args)
 			bool game_has_run = launch_game(error_message, reconnect_requested,
 				game_params, cmd_args);
 
+			// Reset the reconnect_requested flag
+			reconnect_requested = false;
+
 			// If skip_main_menu, we only want to startup once
 			if (skip_main_menu && !first_loop)
 				break;
@@ -336,6 +339,7 @@ bool ClientLauncher::launch_game(std::string &error_message,
 	MainMenuData menudata;
 	menudata.address                         = address;
 	menudata.name                            = playername;
+	menudata.password                        = password;
 	menudata.port                            = itos(game_params.socket_port);
 	menudata.script_data.errormessage        = error_message;
 	menudata.script_data.reconnect_requested = reconnect_requested;