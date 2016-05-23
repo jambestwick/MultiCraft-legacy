minetest/minetest

commit b1965ac20922e3722392114bd63a22b403dcbe98

Clean up rollback

src/rollback.h
@@ -24,31 +24,84 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "irr_v3d.h"
 #include "rollback_interface.h"
 #include <list>
+#include <vector>
+#include "sqlite3.h"
 
 class IGameDef;
 
-class IRollbackManager: public IRollbackReportSink
+class ActionRow;
+class Entity;
+
+class RollbackManager: public IRollbackManager
 {
 public:
-	// IRollbackReportManager
-	virtual void reportAction(const RollbackAction &action) = 0;
-	virtual std::string getActor() = 0;
-	virtual bool isActorGuess() = 0;
-	virtual void setActor(const std::string &actor, bool is_guess) = 0;
-	virtual std::string getSuspect(v3s16 p, float nearness_shortcut,
-	                               float min_nearness) = 0;
+	RollbackManager(const std::string & world_path, IGameDef * gamedef);
+	~RollbackManager();
 
-	virtual ~IRollbackManager() {}
-	virtual void flush() = 0;
-	// Get all actors that did something to position p, but not further than
-	// <seconds> in history
-	virtual std::list<RollbackAction> getNodeActors(v3s16 pos, int range,
-	                time_t seconds, int limit) = 0;
-	// Get actions to revert <seconds> of history made by <actor>
-	virtual std::list<RollbackAction> getRevertActions(const std::string &actor,
-	                time_t seconds) = 0;
+	void reportAction(const RollbackAction & action_);
+	std::string getActor();
+	bool isActorGuess();
+	void setActor(const std::string & actor, bool is_guess);
+	std::string getSuspect(v3s16 p, float nearness_shortcut,
+			float min_nearness);
+	void flush();
+
+	void addAction(const RollbackAction & action);
+	std::list<RollbackAction> getEntriesSince(time_t first_time);
+	std::list<RollbackAction> getNodeActors(v3s16 pos, int range,
+			time_t seconds, int limit);
+	std::list<RollbackAction> getRevertActions(
+			const std::string & actor_filter, time_t seconds);
+
+private:
+	void registerNewActor(const int id, const std::string & name);
+	void registerNewNode(const int id, const std::string & name);
+	int getActorId(const std::string & name);
+	int getNodeId(const std::string & name);
+	const char * getActorName(const int id);
+	const char * getNodeName(const int id);
+	bool createTables();
+	void initDatabase();
+	bool registerRow(const ActionRow & row);
+	const std::list<ActionRow> actionRowsFromSelect(sqlite3_stmt * stmt);
+	ActionRow actionRowFromRollbackAction(const RollbackAction & action);
+	const std::list<RollbackAction> rollbackActionsFromActionRows(
+			const std::list<ActionRow> & rows);
+	const std::list<ActionRow> getRowsSince(time_t firstTime,
+			const std::string & actor);
+	const std::list<ActionRow> getRowsSince_range(time_t firstTime, v3s16 p,
+			int range, int limit);
+	const std::list<RollbackAction> getActionsSince_range(time_t firstTime, v3s16 p,
+			int range, int limit);
+	const std::list<RollbackAction> getActionsSince(time_t firstTime,
+			const std::string & actor = "");
+	void migrate(const std::string & filepath);
+	static float getSuspectNearness(bool is_guess, v3s16 suspect_p,
+		time_t suspect_t, v3s16 action_p, time_t action_t);
+
+
+	IGameDef * gamedef;
+
+	std::string current_actor;
+	bool current_actor_is_guess;
+
+	std::list<RollbackAction> action_todisk_buffer;
+	std::list<RollbackAction> action_latest_buffer;
+
+	std::string database_path;
+	sqlite3 * db;
+	sqlite3_stmt * stmt_insert;
+	sqlite3_stmt * stmt_replace;
+	sqlite3_stmt * stmt_select;
+	sqlite3_stmt * stmt_select_range;
+	sqlite3_stmt * stmt_select_withActor;
+	sqlite3_stmt * stmt_knownActor_select;
+	sqlite3_stmt * stmt_knownActor_insert;
+	sqlite3_stmt * stmt_knownNode_select;
+	sqlite3_stmt * stmt_knownNode_insert;
+
+	std::vector<Entity> knownActors;
+	std::vector<Entity> knownNodes;
 };
-
-IRollbackManager *createRollbackManager(const std::string &filepath, IGameDef *gamedef);
 
 #endif

src/gamedef.h
@@ -30,7 +30,7 @@ class ITextureSource;
 class ISoundManager;
 class IShaderSource;
 class MtEventManager;
-class IRollbackReportSink;
+class IRollbackManager;
 namespace irr { namespace scene {
 	class IAnimatedMesh;
 	class ISceneManager;
@@ -68,7 +68,7 @@ public:
 
 	// Only usable on the server, and NOT thread-safe. It is usable from the
 	// environment thread.
-	virtual IRollbackReportSink* getRollbackReportSink(){return NULL;}
+	virtual IRollbackManager* getRollbackManager(){return NULL;}
 	
 	// Used on the client
 	virtual bool checkLocalPrivilege(const std::string &priv)
@@ -82,7 +82,7 @@ public:
 	ISoundManager* sound(){return getSoundManager();}
 	IShaderSource* shsrc(){return getShaderSource();}
 	MtEventManager* event(){return getEventManager();}
-	IRollbackReportSink* rollback(){return getRollbackReportSink();}
+	IRollbackManager* rollback(){return getRollbackManager();}
 };
 
 #endif

src/inventory.cpp
@@ -183,7 +183,8 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 		legacy_nimap.getName(material, name);
 		if(name == "")
 			name = "unknown_block";
-		name = itemdef->getAlias(name);
+		if (itemdef)
+			name = itemdef->getAlias(name);
 		count = materialcount;
 	}
 	else if(name == "MaterialItem2")
@@ -202,7 +203,8 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 		legacy_nimap.getName(material, name);
 		if(name == "")
 			name = "unknown_block";
-		name = itemdef->getAlias(name);
+		if (itemdef)
+			name = itemdef->getAlias(name);
 		count = materialcount;
 	}
 	else if(name == "node" || name == "NodeItem" || name == "MaterialItem3"
@@ -223,7 +225,8 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 			name = fnd.next(" ");
 		}
 		fnd.skip_over(" ");
-		name = itemdef->getAlias(name);
+		if (itemdef)
+			name = itemdef->getAlias(name);
 		count = stoi(trim(fnd.next("")));
 		if(count == 0)
 			count = 1;
@@ -252,7 +255,8 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 		count = 1;
 		// Then read wear
 		fnd.skip_over(" ");
-		name = itemdef->getAlias(name);
+		if (itemdef)
+			name = itemdef->getAlias(name);
 		wear = stoi(trim(fnd.next("")));
 	}
 	else
@@ -262,7 +266,8 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 			// The real thing
 
 			// Apply item aliases
-			name = itemdef->getAlias(name);
+			if (itemdef)
+				name = itemdef->getAlias(name);
 
 			// Read the count
 			std::string count_str;
@@ -294,9 +299,9 @@ void ItemStack::deSerialize(std::istream &is, IItemDefManager *itemdef)
 		} while(false);
 	}
 
-	if(name.empty() || count == 0)
+	if (name.empty() || count == 0)
 		clear();
-	else if(itemdef->get(name).type == ITEM_TOOL)
+	else if (itemdef && itemdef->get(name).type == ITEM_TOOL)
 		count = 1;
 }
 
@@ -308,11 +313,11 @@ void ItemStack::deSerialize(const std::string &str, IItemDefManager *itemdef)
 
 std::string ItemStack::getItemString() const
 {
-	// Get item string
 	std::ostringstream os(std::ios::binary);
 	serialize(os);
 	return os.str();
 }
+
 
 ItemStack ItemStack::addItem(const ItemStack &newitem_,
 		IItemDefManager *itemdef)

src/inventory.h
@@ -20,12 +20,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #ifndef INVENTORY_HEADER
 #define INVENTORY_HEADER
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include "irrlichttypes.h"
 #include "debug.h"
 #include "itemdef.h"
+#include "irrlichttypes.h"
+#include <istream>
+#include <ostream>
+#include <string>
+#include <vector>
 
 struct ToolCapabilities;
 
@@ -39,8 +40,9 @@ struct ItemStack
 
 	// Serialization
 	void serialize(std::ostream &os) const;
-	void deSerialize(std::istream &is, IItemDefManager *itemdef);
-	void deSerialize(const std::string &s, IItemDefManager *itemdef);
+	// Deserialization.  Pass itemdef unless you don't want aliases resolved.
+	void deSerialize(std::istream &is, IItemDefManager *itemdef = NULL);
+	void deSerialize(const std::string &s, IItemDefManager *itemdef = NULL);
 
 	// Returns the string used for inventory
 	std::string getItemString() const;

src/inventorymanager.cpp
@@ -368,7 +368,7 @@ void IMoveAction::apply(InventoryManager *mgr, ServerActiveObject *player, IGame
 	*/
 	if(!ignore_rollback && gamedef->rollback())
 	{
-		IRollbackReportSink *rollback = gamedef->rollback();
+		IRollbackManager *rollback = gamedef->rollback();
 
 		// If source is not infinite, record item take
 		if(src_can_take_count != -1){
@@ -380,7 +380,7 @@ void IMoveAction::apply(InventoryManager *mgr, ServerActiveObject *player, IGame
 				loc = os.str();
 			}
 			action.setModifyInventoryStack(loc, from_list, from_i, false,
-					src_item.getItemString());
+					src_item);
 			rollback->reportAction(action);
 		}
 		// If destination is not infinite, record item put
@@ -393,7 +393,7 @@ void IMoveAction::apply(InventoryManager *mgr, ServerActiveObject *player, IGame
 				loc = os.str();
 			}
 			action.setModifyInventoryStack(loc, to_list, to_i, true,
-					src_item.getItemString());
+					src_item);
 			rollback->reportAction(action);
 		}
 	}
@@ -632,7 +632,7 @@ void IDropAction::apply(InventoryManager *mgr, ServerActiveObject *player, IGame
 	*/
 	if(!ignore_src_rollback && gamedef->rollback())
 	{
-		IRollbackReportSink *rollback = gamedef->rollback();
+		IRollbackManager *rollback = gamedef->rollback();
 
 		// If source is not infinite, record item take
 		if(src_can_take_count != -1){
@@ -644,7 +644,7 @@ void IDropAction::apply(InventoryManager *mgr, ServerActiveObject *player, IGame
 				loc = os.str();
 			}
 			action.setModifyInventoryStack(loc, from_list, from_i,
-					false, src_item.getItemString());
+					false, src_item);
 			rollback->reportAction(action);
 		}
 	}

src/map.h
@@ -41,7 +41,7 @@ class ServerMapSector;
 class MapBlock;
 class NodeMetadata;
 class IGameDef;
-class IRollbackReportSink;
+class IRollbackManager;
 class EmergeManager;
 class ServerEnvironment;
 struct BlockMakeData;

src/rollback_interface.cpp
@@ -34,6 +34,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #define PP(x) "("<<(x).X<<","<<(x).Y<<","<<(x).Z<<")"
 
+
 RollbackNode::RollbackNode(Map *map, v3s16 p, IGameDef *gamedef)
 {
 	INodeDefManager *ndef = gamedef->ndef();
@@ -42,275 +43,89 @@ RollbackNode::RollbackNode(Map *map, v3s16 p, IGameDef *gamedef)
 	param1 = n.param1;
 	param2 = n.param2;
 	NodeMetadata *metap = map->getNodeMetadata(p);
-	if(metap){
+	if (metap) {
 		std::ostringstream os(std::ios::binary);
 		metap->serialize(os);
 		meta = os.str();
 	}
 }
 
+
 std::string RollbackAction::toString() const
 {
-	switch(type){
-	case TYPE_SET_NODE: {
-		std::ostringstream os(std::ios::binary);
-		os<<"[set_node";
-		os<<" ";
-		os<<"("<<itos(p.X)<<","<<itos(p.Y)<<","<<itos(p.Z)<<")";
-		os<<" ";
-		os<<serializeJsonString(n_old.name);
-		os<<" ";
-		os<<itos(n_old.param1);
-		os<<" ";
-		os<<itos(n_old.param2);
-		os<<" ";
-		os<<serializeJsonString(n_old.meta);
-		os<<" ";
-		os<<serializeJsonString(n_new.name);
-		os<<" ";
-		os<<itos(n_new.param1);
-		os<<" ";
-		os<<itos(n_new.param2);
-		os<<" ";
-		os<<serializeJsonString(n_new.meta);
-		os<<"]";
-		return os.str(); }
-	case TYPE_MODIFY_INVENTORY_STACK: {
-		std::ostringstream os(std::ios::binary);
-		os<<"[modify_inventory_stack";
-		os<<" ";
-		os<<serializeJsonString(inventory_location);
-		os<<" ";
-		os<<serializeJsonString(inventory_list);
-		os<<" ";
-		os<<inventory_index;
-		os<<" ";
-		os<<(inventory_add?"add":"remove");
-		os<<" ";
-		os<<serializeJsonString(inventory_stack);
-		os<<"]";
-		return os.str(); }
+	std::ostringstream os(std::ios::binary);
+	switch (type) {
+	case TYPE_SET_NODE:
+		os << "set_node " << PP(p);
+		os << ": (" << serializeJsonString(n_old.name);
+		os << ", " << itos(n_old.param1);
+		os << ", " << itos(n_old.param2);
+		os << ", " << serializeJsonString(n_old.meta);
+		os << ") -> (" << serializeJsonString(n_new.name);
+		os << ", " << itos(n_new.param1);
+		os << ", " << itos(n_new.param2);
+		os << ", " << serializeJsonString(n_new.meta);
+		os << ')';
+	case TYPE_MODIFY_INVENTORY_STACK:
+		os << "modify_inventory_stack (";
+		os << serializeJsonString(inventory_location);
+		os << ", " << serializeJsonString(inventory_list);
+		os << ", " << inventory_index;
+		os << ", " << (inventory_add ? "add" : "remove");
+		os << ", " << serializeJsonString(inventory_stack.getItemString());
+		os << ')';
 	default:
-		return "none";
+		return "<unknown action>";
 	}
+	return os.str();
 }
 
-void RollbackAction::fromStream(std::istream &is) throw(SerializationError)
-{
-	int c = is.get();
-	if(c != '['){
-		is.putback(c);
-		throw SerializationError("RollbackAction: starting [ not found");
-	}
-	
-	std::string id;
-	std::getline(is, id, ' ');
-	
-	if(id == "set_node")
-	{
-		c = is.get();
-		if(c != '('){
-			is.putback(c);
-			throw SerializationError("RollbackAction: starting ( not found");
-		}
-		// Position
-		std::string px_raw;
-		std::string py_raw;
-		std::string pz_raw;
-		std::getline(is, px_raw, ',');
-		std::getline(is, py_raw, ',');
-		std::getline(is, pz_raw, ')');
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-p ' ' not found");
-		}
-		v3s16 loaded_p(stoi(px_raw), stoi(py_raw), stoi(pz_raw));
-		// Old node
-		std::string old_name;
-		try{
-			old_name = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"old_name: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-old_name ' ' not found");
-		}
-		std::string old_p1_raw;
-		std::string old_p2_raw;
-		std::getline(is, old_p1_raw, ' ');
-		std::getline(is, old_p2_raw, ' ');
-		int old_p1 = stoi(old_p1_raw);
-		int old_p2 = stoi(old_p2_raw);
-		std::string old_meta;
-		try{
-			old_meta = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"old_meta: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-old_meta ' ' not found");
-		}
-		// New node
-		std::string new_name;
-		try{
-			new_name = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"new_name: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-new_name ' ' not found");
-		}
-		std::string new_p1_raw;
-		std::string new_p2_raw;
-		std::getline(is, new_p1_raw, ' ');
-		std::getline(is, new_p2_raw, ' ');
-		int new_p1 = stoi(new_p1_raw);
-		int new_p2 = stoi(new_p2_raw);
-		std::string new_meta;
-		try{
-			new_meta = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"new_meta: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ']'){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-new_meta ] not found");
-		}
-		// Set values
-		type = TYPE_SET_NODE;
-		p = loaded_p;
-		n_old.name = old_name;
-		n_old.param1 = old_p1;
-		n_old.param2 = old_p2;
-		n_old.meta = old_meta;
-		n_new.name = new_name;
-		n_new.param1 = new_p1;
-		n_new.param2 = new_p2;
-		n_new.meta = new_meta;
-	}
-	else if(id == "modify_inventory_stack")
-	{
-		// Location
-		std::string location;
-		try{
-			location = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"location: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-loc ' ' not found");
-		}
-		// List
-		std::string listname;
-		try{
-			listname = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"listname: "<<e.what()<<std::endl;
-			throw e;
-		}
-		c = is.get();
-		if(c != ' '){
-			is.putback(c);
-			throw SerializationError("RollbackAction: after-list ' ' not found");
-		}
-		// Index
-		std::string index_raw;
-		std::getline(is, index_raw, ' ');
-		// add/remove
-		std::string addremove;
-		std::getline(is, addremove, ' ');
-		if(addremove != "add" && addremove != "remove"){
-			throw SerializationError("RollbackAction: addremove is not add or remove");
-		}
-		// Itemstring
-		std::string stack;
-		try{
-			stack = deSerializeJsonString(is);
-		}catch(SerializationError &e){
-			errorstream<<"Serialization error in RollbackAction::fromStream(): "
-					<<"stack: "<<e.what()<<std::endl;
-			throw e;
-		}
-		// Set values
-		type = TYPE_MODIFY_INVENTORY_STACK;
-		inventory_location = location;
-		inventory_list = listname;
-		inventory_index = stoi(index_raw);
-		inventory_add = (addremove == "add");
-		inventory_stack = stack;
-	}
-	else
-	{
-		throw SerializationError("RollbackAction: Unknown id");
-	}
-}
 
 bool RollbackAction::isImportant(IGameDef *gamedef) const
 {
-	switch(type){
-	case TYPE_SET_NODE: {
-		// If names differ, action is always important
-		if(n_old.name != n_new.name)
-			return true;
-		// If metadata differs, action is always important
-		if(n_old.meta != n_new.meta)
-			return true;
-		INodeDefManager *ndef = gamedef->ndef();
-		// Both are of the same name, so a single definition is needed
-		const ContentFeatures &def = ndef->get(n_old.name);
-		// If the type is flowing liquid, action is not important
-		if(def.liquid_type == LIQUID_FLOWING)
-			return false;
-		// Otherwise action is important
-		return true; }
-	default:
+	if (type != TYPE_SET_NODE)
 		return true;
-	}
+	// If names differ, action is always important
+	if(n_old.name != n_new.name)
+		return true;
+	// If metadata differs, action is always important
+	if(n_old.meta != n_new.meta)
+		return true;
+	INodeDefManager *ndef = gamedef->ndef();
+	// Both are of the same name, so a single definition is needed
+	const ContentFeatures &def = ndef->get(n_old.name);
+	// If the type is flowing liquid, action is not important
+	if (def.liquid_type == LIQUID_FLOWING)
+		return false;
+	// Otherwise action is important
+	return true;
 }
+
 
 bool RollbackAction::getPosition(v3s16 *dst) const
 {
-	switch(type){
-	case RollbackAction::TYPE_SET_NODE:
-		if(dst) *dst = p;
+	switch (type) {
+	case TYPE_SET_NODE:
+		if (dst) *dst = p;
 		return true;
-	case RollbackAction::TYPE_MODIFY_INVENTORY_STACK: {
+	case TYPE_MODIFY_INVENTORY_STACK: {
 		InventoryLocation loc;
 		loc.deSerialize(inventory_location);
-		if(loc.type != InventoryLocation::NODEMETA)
+		if (loc.type != InventoryLocation::NODEMETA) {
 			return false;
-		if(dst) *dst = loc.p;
+		}
+		if (dst) *dst = loc.p;
 		return true; }
 	default:
 		return false;
 	}
 }
 
+
 bool RollbackAction::applyRevert(Map *map, InventoryManager *imgr, IGameDef *gamedef) const
 {
-	try{
-		switch(type){
+	try {
+		switch (type) {
 		case TYPE_NOTHING:
 			return true;
 		case TYPE_SET_NODE: {
@@ -321,40 +136,39 @@ bool RollbackAction::applyRevert(Map *map, InventoryManager *imgr, IGameDef *gam
 			MapNode current_node = map->getNodeNoEx(p);
 			std::string current_name = ndef->get(current_node).name;
 			// If current node not the new node, it's bad
-			if(current_name != n_new.name)
+			if (current_name != n_new.name) {
 				return false;
-			/*// If current node not the new node and not ignore, it's bad
-			if(current_name != n_new.name && current_name != "ignore")
-				return false;*/
+			}
 			// Create rollback node
 			MapNode n(ndef, n_old.name, n_old.param1, n_old.param2);
 			// Set rollback node
-			try{
-				if(!map->addNodeWithEvent(p, n)){
-					infostream<<"RollbackAction::applyRevert(): "
-							<<"AddNodeWithEvent failed at "
-							<<PP(p)<<" for "<<n_old.name<<std::endl;
+			try {
+				if (!map->addNodeWithEvent(p, n)) {
+					infostream << "RollbackAction::applyRevert(): "
+						<< "AddNodeWithEvent failed at "
+						<< PP(p) << " for " << n_old.name
+						<< std::endl;
 					return false;
 				}
-				NodeMetadata *meta = map->getNodeMetadata(p);
-				if(n_old.meta != ""){
-					if(!meta){
+				if (n_old.meta.empty()) {
+					map->removeNodeMetadata(p);
+				} else {
+					NodeMetadata *meta = map->getNodeMetadata(p);
+					if (!meta) {
 						meta = new NodeMetadata(gamedef);
-						if(!map->setNodeMetadata(p, meta)){
+						if (!map->setNodeMetadata(p, meta)) {
 							delete meta;
-							infostream<<"RollbackAction::applyRevert(): "
-									<<"setNodeMetadata failed at "
-									<<PP(p)<<" for "<<n_old.name<<std::endl;
+							infostream << "RollbackAction::applyRevert(): "
+								<< "setNodeMetadata failed at "
+								<< PP(p) << " for " << n_old.name
+								<< std::endl;
 							return false;
 						}
 					}
 					std::istringstream is(n_old.meta, std::ios::binary);
 					meta->deSerialize(is);
-				} else {
-					map->removeNodeMetadata(p);
 				}
-				// NOTE: This same code is in scriptapi.cpp
-				// Inform other things that the metadata has changed
+				// Inform other things that the meta data has changed
 				v3s16 blockpos = getContainerPos(p, MAP_BLOCKSIZE);
 				MapEditEvent event;
 				event.type = MEET_BLOCK_NODE_METADATA_CHANGED;
@@ -362,12 +176,14 @@ bool RollbackAction::applyRevert(Map *map, InventoryManager *imgr, IGameDef *gam
 				map->dispatchEvent(&event);
 				// Set the block to be saved
 				MapBlock *block = map->getBlockNoCreateNoEx(blockpos);
-				if(block)
+				if (block) {
 					block->raiseModified(MOD_STATE_WRITE_NEEDED,
-							"NodeMetaRef::reportMetadataChange");
-			}catch(InvalidPositionException &e){
-				infostream<<"RollbackAction::applyRevert(): "
-						<<"InvalidPositionException: "<<e.what()<<std::endl;
+						"NodeMetaRef::reportMetadataChange");
+				}
+			} catch (InvalidPositionException &e) {
+				infostream << "RollbackAction::applyRevert(): "
+					<< "InvalidPositionException: " << e.what()
+					<< std::endl;
 				return false;
 			}
 			// Success
@@ -375,47 +191,46 @@ bool RollbackAction::applyRevert(Map *map, InventoryManager *imgr, IGameDef *gam
 		case TYPE_MODIFY_INVENTORY_STACK: {
 			InventoryLocation loc;
 			loc.deSerialize(inventory_location);
-			ItemStack stack;
-			stack.deSerialize(inventory_stack, gamedef->idef());
+			std::string real_name = gamedef->idef()->getAlias(inventory_stack.name);
 			Inventory *inv = imgr->getInventory(loc);
-			if(!inv){
-				infostream<<"RollbackAction::applyRevert(): Could not get "
-						"inventory at "<<inventory_location<<std::endl;
+			if (!inv) {
+				infostream << "RollbackAction::applyRevert(): Could not get "
+					"inventory at " << inventory_location << std::endl;
 				return false;
 			}
 			InventoryList *list = inv->getList(inventory_list);
-			if(!list){
-				infostream<<"RollbackAction::applyRevert(): Could not get "
-						"inventory list \""<<inventory_list<<"\" in "
-						<<inventory_location<<std::endl;
+			if (!list) {
+				infostream << "RollbackAction::applyRevert(): Could not get "
+					"inventory list \"" << inventory_list << "\" in "
+					<< inventory_location << std::endl;
 				return false;
 			}
-			if(list->getSize() <= inventory_index){
-				infostream<<"RollbackAction::applyRevert(): List index "
-						<<inventory_index<<" too large in "
-						<<"inventory list \""<<inventory_list<<"\" in "
-						<<inventory_location<<std::endl;
+			if (list->getSize() <= inventory_index) {
+				infostream << "RollbackAction::applyRevert(): List index "
+					<< inventory_index << " too large in "
+					<< "inventory list \"" << inventory_list << "\" in "
+					<< inventory_location << std::endl;
 			}
 			// If item was added, take away item, otherwise add removed item
-			if(inventory_add){
+			if (inventory_add) {
 				// Silently ignore different current item
-				if(list->getItem(inventory_index).name != stack.name)
+				if (list->getItem(inventory_index).name != real_name)
 					return false;
-				list->takeItem(inventory_index, stack.count);
+				list->takeItem(inventory_index, inventory_stack.count);
 			} else {
-				list->addItem(inventory_index, stack);
+				list->addItem(inventory_index, inventory_stack);
 			}
 			// Inventory was modified; send to clients
 			imgr->setInventoryModified(loc);
 			return true; }
 		default:
-			errorstream<<"RollbackAction::applyRevert(): type not handled"
-					<<std::endl;
+			errorstream << "RollbackAction::applyRevert(): type not handled"
+				<< std::endl;
 			return false;
 		}
-	}catch(SerializationError &e){
-		errorstream<<"RollbackAction::applyRevert(): n_old.name="<<n_old.name
-				<<", SerializationError: "<<e.what()<<std::endl;
+	} catch(SerializationError &e) {
+		errorstream << "RollbackAction::applyRevert(): n_old.name=" << n_old.name
+				<< ", SerializationError: " << e.what() << std::endl;
 	}
 	return false;
 }

jambestwick/MultiCraft-legacy

commit 30362e5e6c5f2fc2402ed2110e758f088a905ac7

Fix rollback.txt migration
Broken by b1965ac20922e3722392114bd63a22b403dcbe98.
This also prepares the begin and commit statements only once.

src/rollback.cpp
@@ -93,10 +93,10 @@ RollbackManager::RollbackManager(const std::string & world_path,
 	std::string migrating_flag = txt_filename + ".migrating";
 	database_path = world_path + DIR_DELIM "rollback.sqlite";
 
-	initDatabase();
+	bool created = initDatabase();
 
-	if (fs::PathExists(txt_filename) && (fs::PathExists(migrating_flag) ||
-			!fs::PathExists(database_path))) {
+	if (fs::PathExists(txt_filename) && (created ||
+			fs::PathExists(migrating_flag))) {
 		std::ofstream of(migrating_flag.c_str());
 		of.close();
 		migrate(txt_filename);
@@ -250,15 +250,15 @@ bool RollbackManager::createTables()
 }
 
 
-void RollbackManager::initDatabase()
+bool RollbackManager::initDatabase()
 {
 	verbosestream << "RollbackManager: Database connection setup" << std::endl;
 
-	bool needsCreate = !fs::PathExists(database_path);
+	bool needs_create = !fs::PathExists(database_path);
 	SQLOK(sqlite3_open_v2(database_path.c_str(), &db,
 			SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL));
 
-	if (needsCreate) {
+	if (needs_create) {
 		createTables();
 	}
 
@@ -374,6 +374,8 @@ void RollbackManager::initDatabase()
 		);
 	}
 	SQLOK(sqlite3_reset(stmt_knownNode_select));
+
+	return needs_create;
 }
 
 
@@ -672,23 +674,27 @@ void RollbackManager::migrate(const std::string & file_path)
 
 	std::streampos file_size = fh.tellg();
 
-	if (file_size > 10) {
+	if (file_size < 10) {
 		errorstream << "Empty rollback log." << std::endl;
 		return;
 	}
 
 	fh.seekg(0);
 
+	sqlite3_stmt *stmt_begin;
+	sqlite3_stmt *stmt_commit;
+	SQLOK(sqlite3_prepare_v2(db, "BEGIN", -1, &stmt_begin, NULL));
+	SQLOK(sqlite3_prepare_v2(db, "COMMIT", -1, &stmt_commit, NULL));
+
 	std::string bit;
 	int i = 0;
-	int id = 1;
 	time_t start = time(0);
 	time_t t = start;
-	sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
+	SQLRES(sqlite3_step(stmt_begin), SQLITE_DONE);
+	sqlite3_reset(stmt_begin);
 	do {
 		ActionRow row;
-
-		row.id = id;
+		row.id = 0;
 
 		// Get the timestamp
 		std::getline(fh, bit, ' ');
@@ -758,17 +764,24 @@ void RollbackManager::migrate(const std::string & file_path)
 		++i;
 
 		if (time(0) - t >= 1) {
-			sqlite3_exec(db, "COMMIT", NULL, NULL, NULL);
+			SQLRES(sqlite3_step(stmt_commit), SQLITE_DONE);
+			sqlite3_reset(stmt_commit);
 			t = time(0);
 			std::cout
 				<< " Done: " << static_cast<int>((static_cast<float>(fh.tellg()) / static_cast<float>(file_size)) * 100) << "%"
 				<< " Speed: " << i / (t - start) << "/second     \r" << std::flush;
-			sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
+			SQLRES(sqlite3_step(stmt_begin), SQLITE_DONE);
+			sqlite3_reset(stmt_begin);
 		}
 	} while (fh.good());
+	SQLRES(sqlite3_step(stmt_commit), SQLITE_DONE);
+	sqlite3_reset(stmt_commit);
+
+	SQLOK(sqlite3_finalize(stmt_begin));
+	SQLOK(sqlite3_finalize(stmt_commit));
 
 	std::cout
-		<< " Done: 100%                                   " << std::endl
+		<< " Done: 100%                                  " << std::endl
 		<< "Now you can delete the old rollback.txt file." << std::endl;
 }
 

src/rollback.h
@@ -61,7 +61,7 @@ private:
 	const char * getActorName(const int id);
 	const char * getNodeName(const int id);
 	bool createTables();
-	void initDatabase();
+	bool initDatabase();
 	bool registerRow(const ActionRow & row);
 	const std::list<ActionRow> actionRowsFromSelect(sqlite3_stmt * stmt);
 	ActionRow actionRowFromRollbackAction(const RollbackAction & action);