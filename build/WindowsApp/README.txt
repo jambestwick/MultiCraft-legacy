9747b62a14eca50e7665d49bcc807fe8d72bc709
sfan5 <sfan5@live.de>
2017-07-25 18:40:12 +0200

    Update WinApp packaging script (#93)

--- build/WindowsApp/README.txt
+++ build/WindowsApp/README.txt
@@ -1,6 +1,6 @@
 Instructions:
 1) on Linux: sudo apt install unzip zip gettext cmake p7zip-full
-2) on Windows: install DesktopAppConverter and the Windows SDK
+2) on Windows: install DesktopAppConverter and the Windows 10 SDK
 3) Run build.sh to create the ZIP file
 4) Transfer multicraft-windows.zip to Windows and extract it
 5) Edit the settings in the .bat file
--- build/WindowsApp/package.bat
+++ build/WindowsApp/package.bat
@@ -1,16 +1,22 @@
 @echo off
 
-REM ---- change these ----
+REM --- change these ---
 SET folderpath=C:\Users\Doge\Desktop\MultiCraft-1.1.8-dev-win32
 SET outputpath=C:\Users\Doge\Desktop
 SET version=1.1.8.0
 SET publisher=example text
-REM ----------------------
+REM --------------------
 
 rem Find path to DesktopAppConverter
 pushd "%ProgramFiles%\WindowsApps"
 set cmd="dir /b Microsoft.DesktopAppConverter_*"
 for /f "tokens=*" %%i in (' %cmd% ') do set appname=%%i
+popd
+
+rem Find installed Windows 10 SDK version
+pushd "%ProgramFiles(x86)%\Windows Kits\10\bin"
+set cmd="dir /b 10.*"
+for /f "tokens=*" %%i in (' %cmd% ') do set sdkver=%%i
 popd
 
 echo Please wait...
@@ -19,5 +25,8 @@ powershell -NoProfile -NoLogo -ExecutionPolicy Bypass^
  -Installer "%folderpath%" -AppExecutable MultiCraft.exe^
  -Destination "%outputpath%" -PackageName MultiCraft^
  -Publisher "CN=%publisher%" -Version %version%
+"%ProgramFiles(x86)%\Windows Kits\10\bin\%sdkver%\x64\MakeAppx.exe"^
+ pack /l^
+ /d "%outputpath%\MultiCraft\PackageFiles" /p "%outputpath%\MultiCraft.appx"
 
 pause
