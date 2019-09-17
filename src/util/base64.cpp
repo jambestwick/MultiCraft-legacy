3abc8b926ee09e1f796d830f61f6b7a8a7b6b846
MoNTE48 <MoNTE48@mail.ua>
2019-09-17 11:23:29 +0200

    Update base64.cpp

--- src/util/base64.cpp
+++ src/util/base64.cpp
@@ -1,7 +1,7 @@
 /*
 base64.cpp and base64.h
 
-Copyright (C) 2004-2008 René Nyffenegger
+Copyright (C) 2004-2017 René Nyffenegger
 
 This source code is provided 'as-is', without any express or implied
 warranty. In no event will the author be held liable for any damages
@@ -26,13 +26,12 @@ René Nyffenegger rene.nyffenegger@adp-gmbh.ch
 */
 
 #include "base64.h"
-#include <iostream>
+#include <cctype>
 
 static const std::string base64_chars =
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		"abcdefghijklmnopqrstuvwxyz"
 		"0123456789+/";
-
 
 static inline bool is_base64(unsigned char c) {
 	return (isalnum(c) || (c == '+') || (c == '/'));
@@ -48,7 +47,6 @@ bool base64_is_valid(std::string const& s)
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
 	std::string ret;
 	int i = 0;
-	int j = 0;
 	unsigned char char_array_3[3];
 	unsigned char char_array_4[4];
 
@@ -68,13 +66,13 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
 
 	if (i)
 	{
+		int j;
 		for(j = i; j < 3; j++)
 			char_array_3[j] = '\0';
 
 		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
 		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
 		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-		char_array_4[3] = char_array_3[2] & 0x3f;
 
 		for (j = 0; (j < i + 1); j++)
 			ret += base64_chars[char_array_4[j]];
@@ -90,9 +88,8 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
 }
 
 std::string base64_decode(std::string const& encoded_string) {
-	int in_len = encoded_string.size();
+	size_t in_len = encoded_string.size();
 	int i = 0;
-	int j = 0;
 	int in_ = 0;
 	unsigned char char_array_4[4], char_array_3[3];
 	std::string ret;
@@ -101,7 +98,7 @@ std::string base64_decode(std::string const& encoded_string) {
 		char_array_4[i++] = encoded_string[in_]; in_++;
 		if (i ==4) {
 			for (i = 0; i <4; i++)
-				char_array_4[i] = base64_chars.find(char_array_4[i]);
+				char_array_4[i] = base64_chars.find(char_array_4[i]) & 0xff;
 
 			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
 			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
@@ -114,15 +111,12 @@ std::string base64_decode(std::string const& encoded_string) {
 	}
 
 	if (i) {
-		for (j = i; j <4; j++)
-			char_array_4[j] = 0;
-
-		for (j = 0; j <4; j++)
-			char_array_4[j] = base64_chars.find(char_array_4[j]);
+		int j;
+		for (j = 0; j < i; j++)
+			char_array_4[j] = base64_chars.find(char_array_4[j]) & 0xff;
 
 		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
 		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-		char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
 		for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
 	}
