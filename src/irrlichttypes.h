a659963c2404c9ebc97162df05a11cd11cd807ab
MoNTE48 <MoNTE48@mail.ua>
2019-11-12 17:40:21 +0100

    Update irrlichttypes.h from Minetest 5.0

--- src/irrlichttypes.h
+++ src/irrlichttypes.h
@@ -17,8 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#ifndef IRRLICHTTYPES_HEADER
-#define IRRLICHTTYPES_HEADER
+#pragma once
 
 /* Ensure that <stdint.h> is included before <irrTypes.h>, unless building on
  * MSVC, to address an irrlicht issue: https://sourceforge.net/p/irrlicht/bugs/433/
@@ -28,12 +27,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
  *       regardless of the compiler.
  */
 #ifndef _MSC_VER
-#	include <stdint.h>
+#	include <cstdint>
 #endif
 
 #include <irrTypes.h>
 
 using namespace irr;
+
+namespace irr {
 
 // Irrlicht 1.8+ defines 64bit unsigned symbol in irrTypes.h
 #if (IRRLICHT_VERSION_MAJOR == 1 && IRRLICHT_VERSION_MINOR < 8)
@@ -47,6 +48,21 @@ using namespace irr;
 	typedef uint64_t u64;
 #endif
 #endif
+
+#if (IRRLICHT_VERSION_MAJOR == 1 && IRRLICHT_VERSION_MINOR >= 9)
+	namespace core {
+		template <typename T>
+		inline T roundingError();
+
+		template <>
+		inline s16 roundingError()
+		{
+			return 0;
+		}
+	}
+#endif
+
+}
 
 #define S8_MIN  (-0x7F - 1)
 #define S16_MIN (-0x7FFF - 1)
@@ -62,5 +78,3 @@ using namespace irr;
 #define U16_MAX 0xFFFF
 #define U32_MAX 0xFFFFFFFF
 #define U64_MAX 0xFFFFFFFFFFFFFFFF
-
-#endif
