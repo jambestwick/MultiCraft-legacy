fdc983d6f552e35e8f3dea523b03e3d2d6f8f407
MoNTE48 <MoNTE48@mail.ua>
2020-01-17 23:33:12 +0100

    iOS: back LuaJIT [moonjit 2.1.2]

--- build/iOS/deps/luajit.sh
+++ build/iOS/deps/luajit.sh
@@ -1,34 +1,41 @@
 #!/bin/bash -e
 
 . sdk.sh
+export MACOSX_DEPLOYMENT_TARGET=10.13
+
 # MoonJIT
 LUAJIT_VERSION=2.1.2
 
 if [ ! -d moonjit-src ]; then
-	wget https://github.com/moonjit/moonjit/archive/$LUAJIT_VERSION.zip
-	unzip $LUAJIT_VERSION.zip
-	mv moonjit-$LUAJIT_VERSION moonjit-src
-	rm $LUAJIT_VERSION.zip
+    wget https://github.com/moonjit/moonjit/archive/$LUAJIT_VERSION.zip
+    unzip $LUAJIT_VERSION.zip
+    mv moonjit-$LUAJIT_VERSION moonjit-src
+    rm $LUAJIT_VERSION.zip
 fi
 
 cd moonjit-src
 
 # 32-bit
-make amalg -j \
-  DEFAULT_CC=clang HOST_CC="clang -m32 -arch i386" CROSS="$(dirname $IOS_CC)/" \
-  TARGET_FLAGS="-DLUAJIT_DISABLE_FFI -arch armv7 ${IOS_FLAGS_LUA}" TARGET_SYS=iOS
+make -j \
+    DEFAULT_CC=clang HOST_CC="clang -m32 -arch i386" CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+    TARGET_FLAGS="-arch armv7 ${IOS_FLAGS_LUA}" \
+    TARGET_CFLAGS+="-Wno-implicit-function-declaration -fno-omit-frame-pointer"
 mv src/libluajit.a templib_32.a
 make clean
+
 # 64-bit
-make amalg -j \
-  DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" \
-  TARGET_FLAGS="-DLUAJIT_DISABLE_FFI -arch arm64 ${IOS_FLAGS_LUA}" TARGET_SYS=iOS
+make -j \
+    DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+    TARGET_FLAGS="-arch arm64 ${IOS_FLAGS_LUA}" \
+    TARGET_CFLAGS+="-Wno-implicit-function-declaration"
 mv src/libluajit.a templib_64.a
 make clean
+
 # 64-bit [arm64e]
-make amalg -j \
-  DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" \
-  TARGET_FLAGS="-DLUAJIT_DISABLE_FFI -arch arm64e ${IOS_FLAGS_LUA}" TARGET_SYS=iOS
+make -j \
+    DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+    TARGET_FLAGS="-arch arm64e ${IOS_FLAGS_LUA}" \
+    TARGET_CFLAGS+="-Wno-implicit-function-declaration"
 mv src/libluajit.a templib_64e.a
 make clean
 
