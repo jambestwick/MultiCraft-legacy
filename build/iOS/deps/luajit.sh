d61846b15b32e1a9fe636e41fa4146ba7e430b58
MoNTE48 <MoNTE48@mail.ua>
2020-02-06 16:25:32 +0100

    iOS: update LuaJIT version

--- build/iOS/deps/luajit.sh
+++ build/iOS/deps/luajit.sh
@@ -3,39 +3,38 @@
 . sdk.sh
 export MACOSX_DEPLOYMENT_TARGET=10.13
 
-# MoonJIT
-LUAJIT_VERSION=2.1.2
+LUAJIT_VERSION=2.1
 
-if [ ! -d moonjit-src ]; then
-    wget https://github.com/moonjit/moonjit/archive/$LUAJIT_VERSION.zip
-    unzip $LUAJIT_VERSION.zip
-    mv moonjit-$LUAJIT_VERSION moonjit-src
-    rm $LUAJIT_VERSION.zip
+if [ ! -d LuaJIT-src ]; then
+	wget https://github.com/LuaJIT/LuaJIT/archive/v$LUAJIT_VERSION.zip
+	unzip v$LUAJIT_VERSION.zip
+	mv LuaJIT-$LUAJIT_VERSION LuaJIT-src
+	rm v$LUAJIT_VERSION.zip
 fi
 
-cd moonjit-src
+cd LuaJIT-src
 
 # 32-bit
-make -j \
-    DEFAULT_CC=clang HOST_CC="clang -m32 -arch i386" CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
-    TARGET_FLAGS="-arch armv7 ${IOS_FLAGS_LUA}" \
-    TARGET_CFLAGS+="-Wno-implicit-function-declaration -fno-omit-frame-pointer"
+make amalg -j \
+	DEFAULT_CC=clang HOST_CC="clang -m32 -arch i386" CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+	TARGET_FLAGS="-arch armv7 ${IOS_FLAGS_LUA}" \
+	TARGET_CFLAGS+="-Wno-implicit-function-declaration -fno-omit-frame-pointer"
 mv src/libluajit.a templib_32.a
 make clean
 
 # 64-bit
-make -j \
-    DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
-    TARGET_FLAGS="-arch arm64 ${IOS_FLAGS_LUA}" \
-    TARGET_CFLAGS+="-Wno-implicit-function-declaration"
+make amalg -j \
+	DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+	TARGET_FLAGS="-arch arm64 ${IOS_FLAGS_LUA}" \
+	TARGET_CFLAGS+="-Wno-implicit-function-declaration"
 mv src/libluajit.a templib_64.a
 make clean
 
 # 64-bit [arm64e]
-make -j \
-    DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
-    TARGET_FLAGS="-arch arm64e ${IOS_FLAGS_LUA}" \
-    TARGET_CFLAGS+="-Wno-implicit-function-declaration"
+make amalg -j \
+	DEFAULT_CC=clang HOST_CC=clang CROSS="$(dirname $IOS_CC)/" TARGET_SYS=iOS \
+	TARGET_FLAGS="-arch arm64e ${IOS_FLAGS_LUA}" \
+	TARGET_CFLAGS+="-Wno-implicit-function-declaration"
 mv src/libluajit.a templib_64e.a
 make clean
 
