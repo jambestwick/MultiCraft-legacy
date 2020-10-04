d7a7a918c8006fc31632698fece970db52ca5b60
MoNTE48 <MoNTE48@mail.ua>
2020-10-04 20:52:33 +0200

    Increase load screen progress bar

--- src/drawscene.cpp
+++ src/drawscene.cpp
@@ -643,7 +643,7 @@ void draw_load_screen(const std::wstring &text, IrrlichtDevice* device,
 #else
 			float imgRatio = (float) img_size.Height / img_size.Width;
 			u32 imgW = npot2(screensize.X / 2);
-			if (imgW > (screensize.X * 0.6) && imgW >= 1024)
+			if (imgW > (screensize.X * 0.7) && imgW >= 1024)
 				imgW /= 2;
 			u32 imgH = imgW * imgRatio;
 			float imgR = (float) imgW / img_size.Width;
