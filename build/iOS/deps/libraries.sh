6628aaac965d7aaead929c104e30108de0ef7b9b
MoNTE48 <MoNTE48@mail.ua>
2020-11-08 22:35:39 +0100

    iOS: revert 'use libintl from gettext'
    Doesn't work with older iOS versions.

--- build/iOS/deps/libraries.sh
+++ build/iOS/deps/libraries.sh
@@ -3,7 +3,7 @@
 ./irrlicht.sh
 ./freetype.sh
 ./libcurl.sh
-./gettext.sh
+#./gettext.sh ## doesn't work with older iOS versions
 ./openal.sh
 #./luajit.sh ## requires an older version of macOS (with 32-bit support)
 
