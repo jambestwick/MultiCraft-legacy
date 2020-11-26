3766b3f3af81012d0bca9809e77df2da4b45bd0f
Maksim <MoNTE48@mail.ua>
2020-11-26 15:11:56 +0100

    iOS: don't build deps for unused arches

--- build/iOS/deps/sdk.sh
+++ build/iOS/deps/sdk.sh
@@ -7,5 +7,5 @@ osver=10.3
 export IOS_COMPILER=$(xcrun --sdk $sdk --find clang)
 export IOS_CC=$IOS_COMPILER
 export IOS_CXX=$IOS_COMPILER
-export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch armv7s -arch arm64 -arch arm64e -miphoneos-version-min=$osver -fvisibility=hidden -fdata-sections -ffunction-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -Ofast"
+export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch arm64 -miphoneos-version-min=$osver -fvisibility=hidden -fdata-sections -ffunction-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -Ofast"
 export IOS_FLAGS_NOARCH="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -miphoneos-version-min=$osver -fvisibility=hidden -fdata-sections -ffunction-sections -Ofast"
