4aa53d1da472a5d81e050026a61093dae94984fe
Maksim <MoNTE48@mail.ua>
2020-12-02 19:02:54 +0100

    macOS: switch to OpenAL-Soft

--- build/macOS/deps/irrlicht.sh
+++ build/macOS/deps/irrlicht.sh
@@ -3,7 +3,7 @@
 . sdk.sh
 
 [ ! -d irrlicht-src ] && \
-	git clone --depth 1 -b master https://github.com/MoNTE48/Irrlicht irrlicht-src
+	git clone --depth 1 -b ogl-es https://github.com/MoNTE48/Irrlicht irrlicht-src
 
 cd irrlicht-src/source/Irrlicht
 xcodebuild build \
