9ca78091b9d2d222d2cb9519172812e55589bb22
MoNTE48 <MoNTE48@mail.ua>
2022-01-08 17:42:22 +0200

    Fix Android & iOS

--- build/android/native/jni/Android.mk
+++ build/android/native/jni/Android.mk
@@ -248,7 +248,7 @@ LOCAL_SRC_FILES += ../../../lib/jsoncpp/jsoncpp.cpp
 LOCAL_SRC_FILES += ../../../lib/luautf8/lutf8lib.c
 
 # Lua ChaCha Lib
-LOCAL_SRC_FILES += ../../../lib/luachacha/lchacha.c
+LOCAL_SRC_FILES += $(wildcard ../../../lib/luachacha/*.c)
 
 LOCAL_STATIC_LIBRARIES += Curl Gettext Freetype Irrlicht LevelDB OpenAL mbedTLS mbedx509 mbedcrypto Vorbis LuaJIT android_native_app_glue $(PROFILER_LIBS)
 
